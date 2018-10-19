8ea576b4dbe51efbe003f601042378d6a656be38
Ostigland <eko14ost@student.lu.se>
2018-10-19 15:37:52 +0200

    Update RTB_Architecture.py

--- RTB_Architecture.py
+++ RTB_Architecture.py
@@ -8,6 +8,8 @@ Created on Thu Oct  4 14:38:01 2018
 import numpy as np
 import tensorflow as tf
 import os
+import pandas as pd
+import pickle as pickle
 
 ###AGENT-----------------------------------------------------------------------
 
@@ -268,27 +270,31 @@ class agent:
             update_ops.append(op)
 
         self.sess.run(update_ops)
-    
+
 ###ENVIRONMENT-----------------------------------------------------------------
-        
-class environment:
+
+class RTB_train_environment:
     """
-    This class should create and describe the environment, e.g. by defining
-    the actions, states etc. It should track the budget, the budget 
-    consumption rate, the CTR-estimations, winning bids, the KPI-values
-    attained in previous time periods etc.
+    This class should create the training environment. We define the actions,
+    states, etc. The class should also track the budget, the budget
+    consumption rate, the winning rate, and other state-relevant
+    parameters. We also want the class to handle the data and the
+    campaigns.
     """
-    def __init__(self, ctr_estimations, winning_bids):
+    def __init__(self, train_camp_dict):
         """
-        This function should take as inputs the CTR-estimations and the 
-        winning bids as vectors. They will make up the core of the 
-        environment. 
+        The initialization should take all of the bidding data,
+        using a dictionary of data files for different campaigns.
+        We use this type of input in order to be able to separate
+        the campaigns later so that we can train the agent properly.
+        We also want to initialize all state-relevant parameters.
         """
-        self.ctr_estimations = ctr_estimations
-        self.winning_bids = winning_bids
-        
-        """The environment also has to initialize all the state-relevant 
-        features:"""
+        self.train_camp_dict = train_camp_dict
+        self.camp_size = self.train_camp_dict['imp']
+        self.imp_index = self.camp_size
+
+
+        """We initialize all state-relevant features:"""
         self.time_step = 0
         self.budget = 0
         self.n_regulations = 0
@@ -296,168 +302,193 @@ class environment:
         self.cpm = 0
         self.winning_rate = 0
         self.winning_value = 0
+        self.imp_won = 0
         self.termination = True
-        
+
         """We also have to define the space of allowed actions:"""
         self.actions = [-0.08, -0.03, -0.01, 0, 0.01, 0.03, 0.08]
-        
+
         """We also have to define the length of a time-step and the length
         of an episode:"""
         self.step_length = 96
-        self.episode_length = 100
+        self.episode_length = 1000
         """This means that any episode will contain a number of bids corresponding
-        to the product of the step length and the episode length."""
-        
+        to the product of the step length and the episode length. This will be important
+        to consider as we will want to partition the campaigns into episodes or
+        sub-campaigns during training, meaning that we have to partition the budget and
+        split up the impressions."""
+
         """Finally, we have to initialize the Lambda:"""
-        self.Lambda = 0
-        
+        self.Lambda = 1
+
         """and the state:"""
         self.state = [self.time_step, self.budget, self.n_regulations,
-                                 self.budget_consumption_rate, self.cpm, 
-                                 self.winning_rate, self.winning_value, 
-                                 self.Lambda]
-        
+                      self.budget_consumption_rate, self.cpm,
+                      self.winning_rate, self.winning_value,
+                      self.Lambda]
+
+    def get_camp_data(self):
+        """
+        This function should fetch data for training. It should return the
+        winnings bids, CTR estimations and clicks.c
+        """
+        if self.imp_index < self.step_length:
+            bidding_data = self.train_camp_dict['data'].iloc[:self.imp_index, :]
+
+            winning_bids = np.array(bidding_data['winprice'])
+            ctr_estimations = np.array(bidding_data['pctr'])
+            clicks = list(bidding_data['click'])
+
+            self.imp_index = 0
+
+            return winning_bids, ctr_estimations, clicks
+
+        else:
+            bidding_data = self.train_camp_dict['data']\
+                               .iloc[self.imp_index - self.step_length:self.imp_index, :]
+
+            winning_bids = np.array(bidding_data['winprice'])
+            ctr_estimations = np.array(bidding_data['pctr'])
+            clicks = list(bidding_data['click'])
+
+            self.imp_index -= self.step_length
+
+            return winning_bids, ctr_estimations, clicks
+
     def reset(self, budget, initial_Lambda):
         """
-        This function should return an initial state, in the same format
-        as those in the gym environment, i.e. with some state-relevant
-        vector, a numerical reward and a boolean describing whether the
-        episode has terminated or not.
+        This function should return an initial state from which the agent
+        can start interacting with the environment. This means that we need
+        an initial Lambda and an initial budget.
         """
         self.budget = budget
         self.Lambda = initial_Lambda
         self.n_regulations = self.episode_length
 
         self.budget_consumption_rate = 0
-        self.cpm = 0
         self.winning_rate = 0
         self.winning_value = 0
+        self.imp_won = 0
 
         self.state = [self.time_step, self.budget, self.n_regulations,
-                                 self.budget_consumption_rate, self.cpm, 
-                                 self.winning_rate, self.winning_value, 
-                                 self.Lambda]
-        
+                      self.budget_consumption_rate, self.cpm,
+                      self.winning_rate, self.winning_value,
+                      self.Lambda]
+
         reward = 0
-        """SHOULD WE ALSO CONSIDER COST???"""
         self.termination = False
         self.time_step = 0
 
-        return (self.state, reward, self.termination)        
-            
-    
+        return (self.state, reward, self.termination)
+
+    def reward_function(self, ctr, clicks):
+        """
+        Should try to create a reward function which weighs ctr and clicks,
+        i.e. such that both are valued in some way??? Or just weigh with
+        w = 1 for both, such that a click is like ctr = 1 + ctr_est.
+        """
+
     def step(self, action_index):
         """
-        This function should describe what happens when a certain action
-        takes in a certain state. The function takes as an argument the 
-        index of an action, e.g. 0:6 since we have 7 actions.
+        This function should simulate the agent adjusting the Lambda and
+        hence scaling the bids differently. Then, we will plough through
+        a number of bids corresponding to self.step_length and return
+        the new budget, winning rate, etc.
         """
         action = self.actions[action_index]
         self.Lambda = self.Lambda*(1 + action)
-        
-        ctr_estimations = self.ctr_estimations[:self.step_length]
-        bids = ctr_estimations*(1/self.Lambda)
-        winning_bids = self.winning_bids[:self.step_length]
-        
-        self.winning_rate = 0
-        self.winning_value = 0
-        self.cpm = 0
-        
-        new_budget = self.budget
-        for i in range(self.step_length):
-                if (bids[i] > winning_bids[i]) and (winning_bids[i] < new_budget):
-                    new_budget -= winning_bids[i]
-                    self.winning_rate += 1/self.step_length
-                    self.winning_value += ctr_estimations[i]
-                    self.cpm += bids[i]*1000/self.step_length
-                    
-        self.budget_consumption_rate = (self.budget - new_budget)/self.budget
-        self.budget = new_budget            
 
+        winning_bids, ctr_estimations, clicks = self.get_camp_data()
+
+        bids = ctr_estimations*(1/self.Lambda)
+        budget = self.budget
+        self.imp_won = 0
+        self.winning_value = 0
+        self.winning_rate = 0
+
+        for i in range(min(self.imp_index, self.step_length)):
+            if bids[i] > winning_bids[i] and budget > bids[i]:
+                budget -= winning_bids[i]
+                self.imp_won += clicks[i]
+                self.winning_value += ctr_estimations[i]
+                self.winning_rate += 1/self.step_length
+            else:
+                continue
+
+        self.budget_consumption_rate = (budget - self.budget)/self.budget
+        self.budget = budget
         self.n_regulations -= 1
         self.time_step += 1
-        
-        self.ctr_estimations = self.ctr_estimations[self.step_length:]
-        self.winning_bids = self.winning_bids[self.step_length:]
 
-        if self.budget < min(winning_bids[self.time_step:]):
+        if self.time_step == self.episode_length:
             self.termination = True
-            #CONSIDER THIS TERMINATION CONDITION. DOES IT AFFECT THE TRAINIG?
-            #THAT IS, DOES IT GIVE SKEWED EXPERIENCE???
-        
+
         self.state = [self.time_step, self.budget, self.n_regulations,
-                                 self.budget_consumption_rate, self.cpm, 
-                                 self.winning_rate, self.winning_value, 
-                                 self.Lambda]
+                      self.budget_consumption_rate, self.cpm,
+                      self.winning_rate, self.winning_value,
+                      self.Lambda]
 
         reward = self.winning_value
+
         return (self.state, reward, self.termination)
 
+
+# class RTB_test_environment:
+#     """
+#     This class should create the testing environment. It
+#     should in principal have the same functions as the previous
+#     class with the exception that it should also print some
+#     benchmarking metrics.
+#     """
+#     def __init__(self, test_file_dict):
 
 ###DATA------------------------------------------------------------------------
 
 #os.listdir(...)
 
-camp_n = ['1458', '2259', '2261', '2821', '2997', '3358', '3386', '3427', '3476']
-#data_type = ['test.theta', 'train.theta']
-training_files = []
-#test_files = []
+camp_n = ['1458', '2259', '2261', '2821', '2997', '3358']
+#, '3386', '3427', '3476']
 
-for i in camp_n:
-    training_files.append(open(os.path.join\
-                            (os.getcwd(), 
-                             'iPinYou_data\\train.theta'+'_'+i+'.txt')))
-#    test_files.append(open(os.path.join\
-#                            (os.getcwd(), 
-#                             'iPinYou_data\\test.theta'+'_'+i+'.txt')))
-#    info_files.append(open(os.path.join\
-#                            (os.getcwd(),
-#                             'iPinYou_data\\info'+'_'+i+'.txt')))
+def get_data(camp_n):
+    """
+    This function fetches all of the data to be used for training and testing.
+    """
+    train_file_dict = {}
+    test_file_dict = {}
+    data_path = os.path.join(os.getcwd(), 'data\\ipinyou-data')
 
+    for camp in camp_n:
+        test_data = pd.read_csv(data_path + '\\' + camp + '\\' + 'test.theta.txt',
+                                 header=None, index_col=False, sep=' ',names=['click', 'winprice', 'pctr'])
+        train_data = pd.read_csv(data_path + '\\' + camp + '\\' + 'train.theta.txt',
+                                 header=None, index_col=False, sep=' ', names=['click', 'winprice', 'pctr'])
+        camp_info = pickle.load(open(data_path + '\\' + camp + '\\' + 'info.txt', "rb"))
+        test_budget = camp_info['cost_test']
+        train_budget = camp_info['cost_train']
+        test_imp = camp_info['imp_test']
+        train_imp = camp_info['imp_train']
 
-data = []
+        train = {'imp':train_imp, 'budget':train_budget, 'data':train_data}
+        test = {'imp':test_imp, 'budget':test_budget, 'data':test_data}
 
-for i in training_files:
-    T = i.read().split(' ')
-    T.pop(0)
-    data.append(T)
-    
-observation_count = 0
-for i in range(len(camp_n)):
-    observation_count += len(data[i])/2
-    
-ctr_estimations = np.zeros(int(observation_count))
-winning_bids = np.zeros(int(observation_count))
+        train_file_dict[camp] = train
+        test_file_dict[camp] = test
 
-winning_bids_counter = 0
-ctr_estimations_counter = 0
-for i in range(len(data)):
-    for j in range(len(data[i])):
-        if j % 2 == 0:
-            winning_bids[winning_bids_counter] = float(data[i][j])
-            winning_bids_counter += 1
-        else:
-            ctr_estimations[ctr_estimations_counter] = float(data[i][j][:-2])
-            ctr_estimations_counter += 1
+    return train_file_dict, test_file_dict
 
-
+train_file_dict, test_file_dict = get_data(camp_n)
 
 ###EXPERIMENTS-----------------------------------------------------------------
 
 #AGENT parameters
 epsilon_max = 0.9
 epsilon_min = 0.05
-epsilon_decay_rate = 0.0001
+epsilon_decay_rate = 0.000025
 discount_factor = 0.99
 batch_size = 32
 memory_cap = 100000
 update_frequency = 100
 #random_n = 30000
-episodes_n = 100000
-
-#ENVIRONMENT parameters
-budget = 1000000
-initial_Lambda = 0.001
 
 action_size = 7
 state_size = 8
@@ -465,53 +496,63 @@ state_size = 8
 tf.reset_default_graph()
 sess = tf.Session()
 
-
-###WE SHOULD CHANGE THIS INITIALIZATION TO ALSO INCLUDE BUDGET INFORMATION,
-###AND TEST DATA. WE SHOULD ALSO INCLUDE CAMPAIGN INFO. INPUT
-###DICTIONARIES INSTEAD OF LISTS/ARRAYS. THIS WAY, WE CAN MATCH
-###EVERY CAMPAIGN WITH ITS BUDGET TO GET PROPER TRAINING
-###S.T. RESULTS ARE COMPARABLE TO BENCHMARKS!
-rtb_environment = environment(ctr_estimations, winning_bids)
-
 agent = agent(epsilon_max, epsilon_min, epsilon_decay_rate,
               discount_factor, batch_size, memory_cap,
               state_size, action_size, sess)
 
+###TRAINING----------------------------------------------------------------
+
 episode_counter = 1
-total_reward = 0
 global_step_counter = 0
+episode_reward = 0
+total_reward = 0
+avg_win_rate = 0
+budget_scaling = 1/32
+budget_indicator = 0
+budget_bool = False
 
-while episode_counter < episodes_n:
-    state = rtb_environment.reset(budget, initial_Lambda)[0]
-    termination = False
-    episode_reward = 0
-    avg_win_rate = 0
+for i in camp_n:
+    rtb_environment = RTB_train_environment(train_file_dict[i])
+    while rtb_environment.imp_index > 0:
+        initial_Lambda = np.random.normal(0.001, 0.0001)
+        budget = np.random.normal(budget_scaling*train_file_dict[i]['budget'] * rtb_environment.step_length * \
+                 rtb_environment.episode_length / train_file_dict[i]['imp'], 5000)
+        state, reward, termination = rtb_environment.reset(budget, initial_Lambda)
+        while not termination:
+            action = agent.action(state)
+            next_state, reward, termination = rtb_environment.step(action)
 
-    while not termination:
-        action = agent.action(state)
-        next_state, reward, termination = rtb_environment.step(action)
+            episode_reward += reward
+            memory_sample = (action, state, reward, next_state, termination)
+            agent.replay_memory.store_sample(memory_sample)
 
-        episode_reward += reward
-        memory_sample = (action, state, reward, next_state, termination)
-        agent.replay_memory.store_sample(memory_sample)
+            agent.q_learning()
+            if global_step_counter % update_frequency == 0:
+                agent.target_network_update()
 
-        agent.q_learning()
-        if global_step_counter % update_frequency == 0:
-            agent.target_network_update()
+            agent.e_greedy_policy.epsilon_update(global_step_counter)
+            state = next_state
 
-        agent.e_greedy_policy.epsilon_update(global_step_counter)
-        state = next_state
+            if budget_bool == False:
+                if rtb_environment.budget < 10:
+                    budget_indicator = rtb_environment.time_step
+                    budget_bool = True
 
-        global_step_counter += 1
-        avg_win_rate += rtb_environment.winning_rate/rtb_environment.episode_length
+            global_step_counter += 1
+            avg_win_rate += rtb_environment.winning_rate/rtb_environment.episode_length
 
-    total_reward += episode_reward
-    ###THIS SHOULD OUTPUT MORE RELEVANT DATA. IT WOULD BE NICE TO KNOW HOW QUICKLY THE BUDGET IS SPENT, E.G:
-    ###WE WANT eCPC, CPM, BUDGET LEFT, NUMBER OF AUCTIONS, NUMBER OF IMPRESSIONS WON, ETC.
-    print('Episode {} gave reward {} and retained {} in budget out of {} with a win rate of {} and an epsilon {}'
-          .format(episode_counter, episode_reward, rtb_environment.budget, budget, avg_win_rate, agent.e_greedy_policy.epsilon))
-    episode_counter += 1
-    episode_reward = 0
+        #total_reward += episode_reward
+        print('Episode {} gave reward {:.1f} and retained {:.1f} in budget out of {:.2f} with a win rate of {:.2f}'
+              ' and budget depletion at {} and an epsilon {:.2f}'
+              .format(episode_counter, episode_reward, rtb_environment.budget, budget,
+                      avg_win_rate, budget_indicator, agent.e_greedy_policy.epsilon))
+        episode_counter += 1
+        episode_reward = 0
+        avg_win_rate = 0
+        budget_bool = False
+        budget_indicator = 0
+
+###TEST-----------------------------------------------------------
 
 
 sess.close()
